064320dd1ad2b3ef45787cca1b3345a77645e4a3
Alex Lee <alex@alex-lee.site>
2018-11-20 11:04:28 -0800

    ✏️ Switch screen logic in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { Component } from "react";
-import { Grommet } from "grommet";
-import { grommet } from "grommet/themes";
-import { screens } from "./constants";
+import React, { Component } from 'react'
+import { Grommet } from 'grommet'
+import { grommet } from 'grommet/themes'
+import { screens } from './constants'
 
 class App extends Component {
-  state = { screen: "Home" };
+  state = { screen: 'Card' }
+
+  switchScreen = screen => {
+    this.setState(() => {
+      return { screen: screen }
+    })
+    // console.log('screen switched to: ' + screen)
+  }
+
   render() {
-    return <Grommet theme={grommet}>{screens[this.state.screen]}</Grommet>;
+    const Screen = screens[this.state.screen]
+    return (
+      <Grommet theme={grommet}>
+        <Screen switchScreen={this.switchScreen} />
+      </Grommet>
+    )
   }
 }
 
-export default App;
+export default App
