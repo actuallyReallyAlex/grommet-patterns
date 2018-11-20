e91c6426296e37cec6789cd3addacf9c1868e908
Alex Lee <alex@alex-lee.site>
2018-11-19 16:49:11 -0800

    ✨ Create Nav Section Component

---
+++ src/components/Nav.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Box, Anchor, Text, Button } from "grommet";
+import { Grommet } from "grommet-icons";
+
+const Nav = () => {
+  return (
+    <Box
+      direction="row"
+      justify="between"
+      align="center"
+      width="xlarge"
+      alignSelf="center"
+    >
+      <Anchor
+        href="#"
+        onClick={() => alert('clicked')}
+        icon={<Grommet size="large" />}
+        label={<Text size="xlarge">grommet</Text>}
+      />
+      <Button
+        plain
+        onClick={() => alert('clicked')}        
+        type="button"
+        label={
+          <Box
+            pad={{ vertical: "small", horizontal: "medium" }}
+            round="xlarge"
+            background="accent-2"
+          >
+            <Text size="large">patterns</Text>
+          </Box>
+        }
+      />
+    </Box>
+  );
+};
+
+export default Nav;
