722102f22beca584572fbbf7e625db31c98a44ae
Alex Lee <alex@alex-lee.site>
2018-11-19 18:23:28 -0800

    ✨ Create Item Component

---
+++ src/components/Item.js
@@ -0,0 +1,56 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Button, Box, Heading } from "grommet";
+
+const Item = ({ children, name, fill, disabled }) => {
+  return (
+    <Button
+      disabled={disabled}
+      plain
+      fill={fill}
+      onClick={() => alert("clicked")}
+      type="button"
+      label={
+        <Box fill={fill} pad={{ horizontal: "small" }}>
+          <Heading
+            level={3}
+            size="small"
+            margin={{ top: "none", bottom: "small" }}
+          >
+            {name}
+          </Heading>
+          <Box
+            flex
+            background={
+              disabled
+                ? { color: "status-disabled", opacity: "weak" }
+                : { color: "neutral-2", opacity: "weak" }
+            }
+            justify="center"
+            align="center"
+            pad="small"
+            overflow="hidden"
+            round="small"
+            style={{ pointerEvents: "none" }}
+          >
+            {children}
+          </Box>
+        </Box>
+      }
+    />
+  );
+};
+
+Item.propTypes = {
+  children: PropTypes.node.isRequired,
+  disabled: PropTypes.bool,
+  fill: PropTypes.bool,
+  name: PropTypes.string.isRequired
+};
+
+Item.defaultProps = {
+  disabled: false,
+  fill: true
+};
+
+export default Item;
