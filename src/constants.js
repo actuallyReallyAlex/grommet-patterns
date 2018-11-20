2ec3ee09bfbe31d32aae9baf3d920721d03b2fb4
Alex Lee <alex@alex-lee.site>
2018-11-19 16:49:46 -0800

    ✨ Constants File

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./screens/Home";
+
+const screens = {
+  Home: <Home />
+};
+
+const svgSizes = {
+  small: '100',
+  medium: '300',
+  large: '550'
+}
+
+export { screens, svgSizes };
