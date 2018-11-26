abfd5891f19895aa01791e953aa6ecb5cd4ec6be
Alex Lee <alex@alex-lee.site>
2018-11-25 17:26:54 -0800

    📱 Use ResponsiveContext to allow for screen sizing.

--- src/components/Classification.js
+++ src/components/Classification.js
@@ -1,42 +1,49 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Box, Heading, Grid } from "grommet";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Box, Heading, Grid, ResponsiveContext } from 'grommet'
 
 const Classification = ({ children, index = 0, name }) => {
   return (
-    <Box
-      pad={{ vertical: "medium" }}
-      animation={[
-        { type: "zoomIn", duration: 500, delay: 100 + 100 * index },
-        { type: "fadeIn", duration: 500, delay: 100 * index }
-      ]}
-    >
-      <Heading level={2} margin={{ top: "none", horizontal: "small" }}>
-        {name}
-      </Heading>
-      {Grid.available ? (
-        <Grid
-          columns={{ count: "fit", size: "medium" }}
-          rows="small"
-          gap={{ row: "medium" }}
+    <ResponsiveContext.Consumer>
+      {size => (
+        <Box
+          pad={{ vertical: 'medium' }}
+          animation={[
+            { type: 'zoomIn', duration: 500, delay: 100 + 100 * index },
+            { type: 'fadeIn', duration: 500, delay: 100 * index }
+          ]}
         >
-          {children}
-        </Grid>
-      ) : (
-        <Box direction="row" wrap>
-          {React.Children.map(children, child => (
-            <Box basis="medium" pad="small">
-              <Box basis="small">{child}</Box>
+          <Heading level={2} margin={{ top: 'none', horizontal: 'small' }}>
+            {name}
+          </Heading>
+          {Grid.available ? (
+            <Grid
+              columns={{
+                count: 'fit',
+                size: size === 'small' ? 'small' : 'medium'
+              }}
+              rows="small"
+              gap={{ row: 'medium' }}
+            >
+              {children}
+            </Grid>
+          ) : (
+            <Box direction="row" wrap>
+              {React.Children.map(children, child => (
+                <Box basis="medium" pad="small">
+                  <Box basis="small">{child}</Box>
+                </Box>
+              ))}
             </Box>
-          ))}
+          )}
         </Box>
       )}
-    </Box>
-  );
-};
+    </ResponsiveContext.Consumer>
+  )
+}
 
 Classification.propTypes = {
   name: PropTypes.string
-};
+}
 
-export default Classification;
+export default Classification
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,25 +1,29 @@
-import React from "react";
-import { Box } from "grommet";
-import Header from "../components/Header";
-import Wireframe from "./svg/Wireframe";
+import React from 'react'
+import { Box, ResponsiveContext } from 'grommet'
+import Header from '../components/Header'
+import Wireframe from './svg/Wireframe'
 
 const Hero = () => {
   return (
-    <Box>
-      <Box pad="medium" align="center">
-        <Wireframe />
-      </Box>
-      <Header
-        label="common patterns and layouts for applications"
-        summary={
-          <span>
-            Create attractive, reusable user interface elements allowing you to
-            construct your experience with ease.
-          </span>
-        }
-      />
-    </Box>
-  );
-};
+    <ResponsiveContext.Consumer>
+      {size => (
+        <Box>
+          <Box pad="medium" align="center">
+            <Wireframe size={size} />
+          </Box>
+          <Header
+            label="common patterns and layouts for applications"
+            summary={
+              <span>
+                Create attractive, reusable user interface elements allowing you
+                to construct your experience with ease.
+              </span>
+            }
+          />
+        </Box>
+      )}
+    </ResponsiveContext.Consumer>
+  )
+}
 
-export default Hero;
+export default Hero
