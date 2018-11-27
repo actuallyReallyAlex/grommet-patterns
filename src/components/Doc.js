32ba8b2feb0659b6b6488ac6235752d68b561c58
Alex Lee <alex@alex-lee.site>
2018-11-27 09:31:06 -0800

    ✏️ Display a source and demo link for <Doc>

--- src/components/Doc.js
+++ src/components/Doc.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Box, Anchor, Text } from 'grommet'
 import Header from './Header'
 
-const Doc = ({ example, name, description, link }) => {
+const Doc = ({ example, name, description, source, demo }) => {
   return (
     <Box margin={{ bottom: 'large' }} width="xlarge" alignSelf="center">
       <Box
@@ -16,14 +16,24 @@ const Doc = ({ example, name, description, link }) => {
         {example}
       </Box>
       <Header label={name} summary={description} />
-      {link && (
-        <Anchor
-          alignSelf="center"
-          href={link.url}
-          target="_blank"
-          label={<Text size="large">{link.label}</Text>}
-        />
-      )}
+      <Box alignSelf="center" direction="row-responsive" gap="large">
+        {source && (
+          <Anchor
+            alignSelf="center"
+            href={source}
+            target="_blank"
+            label={<Text size="large">Source</Text>}
+          />
+        )}
+        {demo && (
+          <Anchor
+            alignSelf="center"
+            href={demo}
+            target="_blank"
+            label={<Text size="large">Demo</Text>}
+          />
+        )}
+      </Box>
       <Box
         margin={{ top: 'large' }}
         border={{ side: 'top', size: 'medium', color: 'brand' }}
@@ -36,7 +46,8 @@ Doc.propTypes = {
   example: PropTypes.node,
   name: PropTypes.string,
   description: PropTypes.node,
-  link: PropTypes.object
+  source: PropTypes.string,
+  demo: PropTypes.string
 }
 
 export default Doc
